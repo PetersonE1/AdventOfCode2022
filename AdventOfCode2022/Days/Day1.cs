81af57d6d247d1b7770d2b10e7fe46019f4e771a
Erik Alexander Peterson <PetersonE_1@LIVE.COM>
2022-12-01 16:15:52 -0500

    fixed day 1 part 1 because I realized it was asking for the calories of the highest elf and not which elf was holding those calories

--- AdventOfCode2022/Days/Day1.cs
+++ AdventOfCode2022/Days/Day1.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode2022.Days
         public static int CalculateMostCalories(string input)
         {
             int[] calories = input.Split("\n\r\n").Select(s => s.Split('\n').Select(n => Convert.ToInt32(n)).Sum()).ToArray();
-            return calories.FindIndexOf(calories.Max()) + 1;
+            return calories.Max();
         }
     }
 }
