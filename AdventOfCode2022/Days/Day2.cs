d2c068206db8cf4ecef6ff84c33c517bb235d8c4
PetersonE1 <PetersonE_1@live.com>
2022-12-02 00:24:37 -0500

    day 2

---
+++ AdventOfCode2022/Days/Day2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Days
+{
+    internal static class Day2
+    {
+        public static int GameResult(string input)
+        {
+            char[][] plan = input.Split('\n').Select(s => s.ToCharArray().Where(c => c != ' ').ToArray()).ToArray();
+            int score = 0;
+            foreach (char[] c in plan)
+            {
+                switch (c[1])
+                {
+                    case 'X': c[1] = 'A'; score += 1; break;
+                    case 'Y': c[1] = 'B'; score += 2; break;
+                    case 'Z': c[1] = 'C'; score += 3; break;
+                }
+                if (c[0] == c[1])
+                {
+                    score += 3;
+                    continue;
+                }
+                if (c[0] == 'A')
+                {
+                    score += (c[1] == 'B' ? 6 : 0);
+                    continue;
+                }
+                if (c[0] == 'B')
+                {
+                    score += (c[1] == 'A' ? 0 : 6);
+                    continue;
+                }
+                if (c[0] == 'C')
+                {
+                    score += (c[1] == 'A' ? 6 : 0);
+                    continue;
+                }
+            }
+            return score;
+        }
+
+        public static int TrueGameResult(string input)
+        {
+            char[][] plan = input.Split('\n').Select(s => s.ToCharArray().Where(c => c != ' ').ToArray()).ToArray();
+            int score = 0;
+            foreach (char[] c in plan)
+            {
+                switch (c[1])
+                {
+                    case 'X':
+                        { 
+                            switch (c[0])
+                            {
+                                case 'A': score += 3; break;
+                                case 'B': score += 1; break;
+                                case 'C': score += 2; break;
+                            }
+                        } 
+                        break;
+                    case 'Y':
+                        {
+                            switch (c[0])
+                            {
+                                case 'A': score += 1; break;
+                                case 'B': score += 2; break;
+                                case 'C': score += 3; break;
+                            }
+                        }
+                        score += 3; break;
+                    case 'Z': 
+                        {
+                            switch (c[0])
+                            {
+                                case 'A': score += 2; break;
+                                case 'B': score += 3; break;
+                                case 'C': score += 1; break;
+                            }
+                        }
+                        score += 6; break;
+                }
+            }
+            return score;
+        }
+    }
+}
--- AdventOfCode2022/Runner.cs
+++ AdventOfCode2022/Runner.cs
@@ -14,6 +14,7 @@ namespace AdventOfCode2022
             switch (args[0])
             {
                 case "1": RunDay1(isFirst); break;
+                case "2": RunDay2(isFirst); break;
                 default: break;
             }
         }
@@ -22,6 +23,15 @@ namespace AdventOfCode2022
         {
             string input = Extensions.LoadInput("Day1");
             Console.WriteLine(Days.Day1.CalculateMostCalories(input, firstDay));
+        }
+
+        private static void RunDay2(bool firstDay)
+        {
+            string input = Extensions.LoadInput("Day2");
+            if (firstDay)
+                Console.WriteLine(Days.Day2.GameResult(input));
+            else
+                Console.Write(Days.Day2.TrueGameResult(input));
         }
     }
 }
