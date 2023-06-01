a974145a3a1dcc93ea045931a9e8024fa1d6dbc2
Erik Alexander Peterson <PetersonE_1@LIVE.COM>
2023-06-01 09:45:18 -0400

    day 15 complete

--- AdventOfCode2022/Days/Day15.cs
+++ AdventOfCode2022/Days/Day15.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +13,6 @@ namespace AdventOfCode2022.Days
 {
     internal static class Day15
     {
-        // Adjust to work in ranges
-        // width at sensor = manhattan distance
-        // width -= 2 each y away
-        // at each y, check multiple ranges for continuity
-        // if continuity is broken, second range minimum - 1 = beacon
         public static int BeaconMissing(string input, int line_to_check)
         {
             string[] lines = input.Split("\r\n");
@@ -48,7 +46,7 @@ namespace AdventOfCode2022.Days
             return blockedCells;
         }
 
-        public static int FindBeacon(string input, int search_area)
+        public static ulong FindBeacon(string input, int search_area)
         {
             string[] lines = input.Split("\r\n");
             Dictionary<Vector2Int, Beacon> sensors = new Dictionary<Vector2Int, Beacon>();
@@ -60,16 +58,50 @@ namespace AdventOfCode2022.Days
                 sensors.Add(key, new Beacon(value, key.Manhattan(value)));
             }
 
-            for (int x = 0; x <= search_area; x++)
+            List<Tuple<int, Tuple<int, int>>> ranges = new List<Tuple<int, Tuple<int, int>>>();
+            foreach (var pair in sensors)
             {
-                for (int y = 0; y <= search_area; y++)
+                int y = pair.Key.y;
+                int x = pair.Key.x;
+                for (int offset = 0; offset < pair.Value.distance; offset++)
                 {
-                    if (!IsBlocked(new Vector2Int(x, y), sensors, false))
-                    {
-                        Console.WriteLine($"{x}, {y}");
-                        return x * 4000000 + y;
-                    }
+                    int yMin = y - offset;
+                    int yMax = y + offset;
+                    int xOffset = pair.Value.distance - offset;
+                    int xMin = x - xOffset;
+                    int xMax = x + xOffset;
+                    if (xMin < 0)
+                        xMin = 0;
+                    if (xMax > search_area)
+                        xMax = search_area;
+                    Tuple<int, int> r = new Tuple<int, int>(xMin, xMax);
+                    if (yMin >= 0)
+                        ranges.Add(new Tuple<int, Tuple<int, int>>(y - offset, r));
+                    if (yMax <= search_area)
+                        ranges.Add(new Tuple<int, Tuple<int, int>>(y + offset, r));
                 }
+            }
+
+            ranges = ranges.Distinct().ToList();
+            ranges.Sort(new TupleIntKeySort());
+
+            int cachedY = -1;
+            Tuple<int, int> cachedRange = new Tuple<int, int>(0, 0);
+            foreach (Tuple<int, Tuple<int, int>> range in ranges)
+            {
+                if (cachedY != range.Item1)
+                {
+                    cachedY = range.Item1;
+                    cachedRange = range.Item2;
+                    continue;
+                }
+                if ((range.Item2.Item1 > cachedRange.Item2 + 1 && range.Item2.Item2 > cachedRange.Item2 + 1) || (range.Item2.Item1 + 1 < cachedRange.Item1 && range.Item2.Item2 + 1 < cachedRange.Item1))
+                {
+                    return (ulong)(range.Item2.Item1 - 1) * 4000000 + (uint)range.Item1;
+                }
+                if (range.Item2.Item1 >= cachedRange.Item1 && range.Item2.Item2 <= cachedRange.Item2)
+                    continue;
+                cachedRange = range.Item2;
             }
 
             return 0;
@@ -113,6 +145,19 @@ namespace AdventOfCode2022.Days
         public int GetHashCode([DisallowNull] Beacon obj)
         {
             return obj.pos.GetHashCode();
+        }
+    }
+
+    internal class TupleIntKeySort : IComparer<Tuple<int, Tuple<int, int>>>
+    {
+        public int Compare(Tuple<int, Tuple<int, int>>? x, Tuple<int, Tuple<int, int>>? y)
+        {
+            int result = x.Item1 - y.Item1;
+            if (result == 0)
+            {
+                return x.Item2.Item1 - y.Item2.Item1;
+            }
+            return result;
         }
     }
 }
