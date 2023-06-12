f2eb3cabe23577918a68f5845c7bed6251f7b64c
Erik Alexander Peterson <PetersonE_1@LIVE.COM>
2023-06-12 08:41:24 -0400

    day 16 input parsing

--- AdventOfCode2022/Days/Day16.cs
+++ AdventOfCode2022/Days/Day16.cs
@@ -10,7 +10,50 @@ namespace AdventOfCode2022.Days
     {
         public static int PressureRelease(string input)
         {
+            Valve[] valves = ParseInput(input);
             return 0;
+        }
+
+        public static Valve[] ParseInput(string input)
+        {
+            List<Valve> valves = new List<Valve>();
+            foreach (string line in input.Split("\r\n"))
+            {
+                string valve = string.Empty;
+                int flow = 0;
+                List<string> outs = new List<string>();
+
+                string[] sections = line.Split(' ');
+                for (int i = 0; i < sections.Length; i++)
+                {
+                    if (i == 1)
+                        valve = sections[i];
+                    if (i == 4)
+                        flow = Convert.ToInt32(sections[i].Substring(5, sections[i].Length - 6));
+                    if (i > 8)
+                    {
+                        string s = sections[i].Replace(",", string.Empty);
+                        outs.Add(s);
+                    }
+                }
+
+                Valve v = new Valve(valve, flow, outs.ToArray());
+            }
+            return valves.ToArray();
+        }
+    }
+
+    internal class Valve
+    {
+        public string id;
+        public int flow_rate;
+        public string[] valves;
+
+        public Valve(string id, int flow_rate, string[] valves)
+        {
+            this.id = id;
+            this.flow_rate = flow_rate;
+            this.valves = valves;
         }
     }
 }
